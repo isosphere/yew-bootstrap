d0884f7c39a21e5d453e8a0857ed57a7d0fbb51d
Matthew Scheffel <matt@dataheck.com>
2022-11-28 21:23:05 -0400

    Implement `include_cdn_js()`

--- src/util/include.rs
+++ src/util/include.rs
@@ -11,6 +11,24 @@ pub fn include_cdn() -> VNode {
     }
 }
 
+
+pub fn include_cdn_js() -> VNode {
+    html! {
+        <>
+            <link data-trunk={"true"} rel="copy-file" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js.map" />
+            <script
+                src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+                data-trunk={"true"}
+                integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+                crossorigin="anonymous"
+            >
+            </script>
+        </>
+    }
+}
+
+
+
 pub fn include_inline() -> VNode {
     html! {
         <style>
