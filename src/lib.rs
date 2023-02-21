82c6faf6ebdfc3ad54e43c076e45fb313750899d
Cédric Airaud <cairaud@gmail.com>
2023-02-21 21:44:08 +0100

    Add Bootstrap form fields
    Most of the inputs are available with most of the
    display options.

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,7 @@
 
 /// Components supported by this crate are listed here
 pub mod component;
+/// Form inputs and controls
+pub mod form;
 /// Helper functions and types are listed here, for example [util::Color] and the [util::include_cdn] function.
 pub mod util;
