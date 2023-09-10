bdb783bc03064fab339fb9e6d114f4b8d4310f76
Alex Kazik <alex@kazik.de>
2023-09-10 20:08:30 +0200

    Use BI in NavBar
    This is a breaking change!

--- examples/forms/src/main.rs
+++ examples/forms/src/main.rs
@@ -157,7 +157,7 @@ impl Component for Model {
         let brand = BrandType::BrandIcon {
             text: AttrValue::from("Yew Bootstrap"),
             url: Some(AttrValue::from("https://yew.rs")),
-            icon: AttrValue::from("rocket")
+            icon: BI::ROCKET,
         };
 
         html! {
