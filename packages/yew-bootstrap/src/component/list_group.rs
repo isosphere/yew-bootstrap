6e08181b8e93b560b60d1bb4b697b30a50125d0a
Rune Tynan <runetynan@gmail.com>
2023-10-30 14:00:00 -0400

    Add documentation

--- packages/yew-bootstrap/src/component/list_group.rs
+++ packages/yew-bootstrap/src/component/list_group.rs
@@ -2,9 +2,12 @@ use yew::prelude::*;
 use crate::util::Color;
 use super::*;
 
+/// The variant style of a [ListGroup]
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum ListGroupVariant {
+    /// Default style, with rounded corners and outer borders.
     Default,
+    /// Flush style, removes rounding and border.
     Flush,
 }
 
@@ -14,27 +17,61 @@ impl Default for ListGroupVariant {
     }
 }
 
+/// A size threshold to trigger a property at
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum SizeTrigger {
+    /// Trigger at the given size boundary
     AtSize(ContainerSize),
+    /// Always active
     Always,
+    /// Never active
     Never,
 }
 
+/// # Properties of [ListGroup]
 #[derive(Properties, Clone, PartialEq)]
 pub struct ListGroupProps {
+    /// Inner items (displayed in the [ListGroup]).
     #[prop_or_default]
     pub children: ChildrenWithProps<ListGroupItem>,
+    /// Extra CSS classes to include, in addition to the defaults.
     #[prop_or_default]
     pub class: Classes,
+    /// Display variant to use, see [ListGroupVariant] for all options.
     #[prop_or_default]
     pub variant: ListGroupVariant,
+    /// Whether to number the list.
     #[prop_or_default]
     pub numbered: bool,
+    /// Control when the list is displayed horizontally. Always, or at a certain container size.
     #[prop_or(SizeTrigger::Never)]
     pub horizontal: SizeTrigger,
 }
 
+/// # ListGroup component
+/// A list of items with various properties, including support for numbering, actions, and item
+/// colors.
+///
+/// Items are expected to be instances of [ListGroupItem]
+///
+/// See [ListGroupProps] for a list of properties.
+///
+/// ## Example
+/// Example of a simple list group:
+///
+/// ```rust
+/// use yew::prelude::*;
+/// use yew_bootstrap::component::{ListGroup, ListGroupItem};
+/// fn test() -> Html {
+///     html! {
+///         <ListGroup>
+///             <ListGroupItem>{"First"}</ListGroupItem>
+///             <ListGroupItem active=true>{"Second"}</ListGroupItem>
+///             <ListGroupItem>{"Third"}</ListGroupItem>
+///         </ListGroup>
+///     }
+/// }
+///
 #[function_component]
 pub fn ListGroup(props: &ListGroupProps) -> Html {
     let mut classes = Classes::from("list-group");
@@ -61,26 +98,57 @@ pub fn ListGroup(props: &ListGroupProps) -> Html {
     }
 }
 
+/// # Properties for [ListGroupItem]
 #[derive(Properties, Clone, PartialEq)]
 pub struct ListGroupItemProps {
+    /// Inner components (displayed in the [ListGroupItem]).
     #[prop_or_default]
     pub children: Children,
+    /// Extra CSS classes to include, in addition to the defaults.
     #[prop_or_default]
     pub class: Classes,
+    /// Optional color to use for the background and border of this item.
     #[prop_or_default]
     pub style: Option<Color>,
+    /// Whether this item is the currently active one
     #[prop_or_default]
     pub active: bool,
+    /// Whether this item is disabled
     #[prop_or_default]
     pub disabled: bool,
+    /// Whether this item is actionable, enables hover and click reactivity.
     #[prop_or_default]
     pub action: bool,
+    /// URL to direct to when the list item is clicked
     #[prop_or_default]
     pub url: Option<AttrValue>,
+    /// Event called when the list item is clicked
     #[prop_or_default]
     pub onclick: Callback<MouseEvent>,
 }
 
+/// # ListGroupItem
+/// Used with [ListGroup] to create grouped lists of items.
+///
+/// See [ListGroupItemProps] for a list of properties.
+///
+/// ## Example
+/// Example of a simple list group:
+///
+/// ```rust
+/// use yew::prelude::*;
+/// use yew_bootstrap::component::{ListGroup, ListGroupItem};
+/// use yew_bootstrap::util::Color;
+/// fn test() -> Html {
+///     html! {
+///         <ListGroup>
+///             <ListGroupItem style={Color::Light}>{"First"}</ListGroupItem>
+///             <ListGroupItem active=true>{"Second"}</ListGroupItem>
+///             <ListGroupItem disabled=true>{"Third"}</ListGroupItem>
+///         </ListGroup>
+///     }
+/// }
+///
 #[function_component]
 pub fn ListGroupItem(props: &ListGroupItemProps) -> Html {
     let mut classes = Classes::from("list-group-item");
